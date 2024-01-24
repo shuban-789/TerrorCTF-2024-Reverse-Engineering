ec51580cfabfd010c99be7d3c2a5ee787b34443e
Shuban Pal <67974101+shuban-789@users.noreply.github.com>
2024-01-24 12:59:12 -0800

    Update jumbler.py

--- Reverse Engineering/ascii-jumbler/jumbler.py
+++ Reverse Engineering/ascii-jumbler/jumbler.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-jumbled_data = [611, 101, 411, 411, 111, 411, 76, 84, 70, 321, 80, 411, 84, 95, 52, 611, 95, 711, 110, 74, 711, 901, 98, 801, 94, 110, 71, 33, 33, 95, 65, 52, 51, 50, 75, 521]
+jumbled_data = [[611], [101, 0], [411], [411], [111, 0], [411], [76, 1], [84, 0], [70], [321], [80], [411], [84, 1], [95, 0], [52], [611], [95, 0], [711], [110], [74, 0], [711], [901], [98, 0], [801], [94, 1], [110], [71], [33, 0], [33, 0], [95, 0], [65, 1], [52], [51], [50], [75, 1], [521]]
 
-# REMEMBER: The reverse of a number 500, 50, and 5 is all 5. When dealing a number like that, think about which one would work...
 def jumble(string):
+  bigset = []
   value = []
   thing = [[] for i in range(len(string))]
   for i in range(len(string)):
@@ -10,7 +10,15 @@ def jumble(string):
     thing[i].append(int(str(ord(string[i]))[::-1]))
   for j in range(len(thing)):
     value.append(max(thing[j]))
-  return value
+    if 33 <= thing[j][0] <= 126:
+      if 33 <= int(str(thing[j][0])[::-1]) <= 126:
+        if max(thing[j]) != thing[j][0]:
+          value.append(1) # Swapped num with reverse in range
+        else:
+          value.append(0) # Not swapped num with reverse in range
+    bigset.append(value)
+    value = []
+  return bigset
 
 def main():
   print("Think you unjumbled the flag?")
